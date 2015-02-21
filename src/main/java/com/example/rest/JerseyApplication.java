da99228e9ba6ddb7500606984173bec924f8c07d
Ramana Duriseti <rduriseti@gmail.com>
2015-02-21 01:40:11 -0600

    first commit

---
+++ src/main/java/com/example/rest/JerseyApplication.java
@@ -0,0 +1,10 @@
+package com.example.rest;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class JerseyApplication extends ResourceConfig {
+    public JerseyApplication() {
+       register(JacksonFeature.class);
+    }
+}
